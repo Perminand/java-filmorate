18604be1f44ba753a66388ac918b69db8ab8896f
Андрей <Perminand@yandex.ru>
2024-04-11 19:06:55 +0300

    feat: Создать класс

---
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ConditionsNotMetException.java
@@ -0,0 +1,7 @@
+package ru.yandex.practicum.filmorate.exception;
+
+public class ConditionsNotMetException extends Throwable {
+    public ConditionsNotMetException(String s) {
+        super(s);
+    }
+}
