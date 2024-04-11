ee7b2ecf0cdd4cd5589eca84a7a3aa2c809343a9
Андрей <Perminand@yandex.ru>
2024-04-11 19:15:24 +0300

    feat: Создать матод update

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -36,6 +36,22 @@ public class FilmController {
         film.setId(getNextId());
         return films.put(film.getId(),film);
     }
+
+    public Film update(@RequestBody Film film) throws ConditionsNotMetException {
+        if (film.getName() == null || film.getName().isBlank()) {
+            throw new ConditionsNotMetException("название не может быть пустым");
+        }
+        if (film.getDescription().length() > 200) {
+            throw new ConditionsNotMetException("максимальная длина описания — 200 символов");
+        }
+        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            throw new ConditionsNotMetException("дата релиза — не раньше 28 декабря 1895 года");
+        }
+        if (film.getDuration().getSeconds() < 0) {
+            throw new ConditionsNotMetException("продолжительность фильма должна быть положительным числом.");
+        }
+        return films.replace(film.getId(), film);
+    }
     private long getNextId() {
         long currentMaxId = films.keySet()
                 .stream()
