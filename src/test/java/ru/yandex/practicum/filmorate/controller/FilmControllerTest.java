8fd508c47642028ac160b9c90a782ce7c9a57bad
Андрей <Perminand@yandex.ru>
2024-04-14 07:30:18 +0300

    feat: create method createFilmNullFiled, add final to parameters

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -1,14 +1,9 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import com.sun.net.httpserver.HttpServer;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.model.Film;
 
-import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.time.Duration;
 import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FilmControllerTest {
     private static final int PORT = 8080;
-    static FilmController filmController = new FilmController();
+    static final FilmController filmController = new FilmController();
 
     @Test
     void createFilmOk() throws ConditionsNotMetException {
@@ -25,17 +20,23 @@ class FilmControllerTest {
                 .name("name")
                 .description("description")
                 .releaseDate(LocalDate.now())
-                .duration(Duration.ZERO).build();
+                .duration(0).build();
         filmController.validate(validFilm);
     }
 
     @Test
-    void createFilmNameFiled() throws ConditionsNotMetException {
-        Film validFilm = Film.builder()
+    void createFilmNullFiled() {
+        final Film validFilm = null;
+        NullPointerException exception = assertThrows(NullPointerException.class,
+                () -> filmController.validate(validFilm));
+    }
+    @Test
+    void createFilmNameFiled() {
+        final Film validFilm = Film.builder()
                 .name("")
                 .description("description")
                 .releaseDate(LocalDate.now())
-                .duration(Duration.ZERO).build();
+                .duration(0).build();
         ConditionsNotMetException exception = assertThrows(ConditionsNotMetException.class,
                 () -> filmController.validate(validFilm));
         assertEquals("Название не может быть пустым",
@@ -43,16 +44,16 @@ class FilmControllerTest {
     }
 
     @Test
-    void createFilmDescriptionFiled() throws ConditionsNotMetException {
-        StringBuilder s = new StringBuilder(""); // Создаем строку больше 201 символа
+    void createFilmDescriptionFiled() {
+        final StringBuilder s = new StringBuilder(); // Создаем строку больше 201 символа
         for (int i=0;i<201;i++){
             s.append("t"); // Рандомный символ
         }
-        Film validFilm = Film.builder()
+        final Film validFilm = Film.builder()
                 .name("name")
                 .description(new String(s))
                 .releaseDate(LocalDate.now())
-                .duration(Duration.ZERO).build();
+                .duration(0).build();
         ConditionsNotMetException exception = assertThrows(ConditionsNotMetException.class,
                 () -> filmController.validate(validFilm));
         assertEquals("Максимальная длина описания — 200 символов",
@@ -60,24 +61,24 @@ class FilmControllerTest {
     }
 
     @Test
-    void createFilmReleaseDateFiled() throws ConditionsNotMetException {
-        Film validFilm = Film.builder()
+    void createFilmReleaseDateFiled() {
+        final Film validFilm = Film.builder()
                 .name("name")
                 .description("description")
                 .releaseDate(LocalDate.of(1894,1,1))
-                .duration(Duration.ZERO).build();
+                .duration(0).build();
         ConditionsNotMetException exception = assertThrows(ConditionsNotMetException.class,
                 () -> filmController.validate(validFilm));
         assertEquals("Дата релиза — не раньше 28 декабря 1895 года",
                 exception.getMessage());
     }
     @Test
-    void createFilmDurationFiled() throws ConditionsNotMetException {
-        Film validFilm = Film.builder()
+    void createFilmDurationFiled() {
+        final Film validFilm = Film.builder()
                 .name("name")
                 .description("description")
                 .releaseDate(LocalDate.of(1897,1,1))
-                .duration(Duration.ofSeconds(-1)).build();
+                .duration(-1).build();
         ConditionsNotMetException exception = assertThrows(ConditionsNotMetException.class,
                 () -> filmController.validate(validFilm));
         assertEquals("Продолжительность фильма должна быть положительным числом",
