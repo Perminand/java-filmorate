c7f205643ab513c2bc3a6856c2bb96188cc0e1c0
Андрей <Perminand@yandex.ru>
2024-04-14 07:28:18 +0300

    refactor: add final parameters

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -10,59 +10,59 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @RestController
 @RequestMapping("/films")
 @Slf4j
 public class FilmController {
     private static final int MAX_lENGTH_DESCRIPTION = 200;
+    private static final LocalDate DATE_MARK = LocalDate.of(1895, 12, 28);
     private final Map<Long, Film> films = new HashMap<>();
 
     @GetMapping
-    public Collection<Film> getAll (){
+    public Collection<Film> getAll() {
         return films.values();
     }
 
-    @PostMapping("/film")
+    @PostMapping
     public Film create(@RequestBody Film film) throws ConditionsNotMetException {
         validate(film);
         film.setId(getNextId());
-        Film newFilm = films.put(film.getId(),film);
-        log.debug("film create" + newFilm);
-        return newFilm;
+        films.put(film.getId(), film);
+        log.debug("film create" + film);
+        return film;
     }
 
-    @PutMapping("/film")
+    @PutMapping
     public Film update(@RequestBody Film film) throws ConditionsNotMetException {
         validate(film);
-        Film newFilm = films.replace(film.getId(), film);
-        log.debug("film update" + newFilm);
-        return newFilm;
+        films.replace(film.getId(), film);
+        log.debug("film update" + film);
+        return film;
     }
 
-    public void validate(Film film) throws ConditionsNotMetException {
-        log.info("Запущена валидация");
+    public void validate(final Film film) throws ConditionsNotMetException {
         if (film.getName() == null || film.getName().isBlank()) {
-            String s = "Название не может быть пустым";
+            final String s = "Название не может быть пустым";
             log.info("Вызвано исключение: " + s + " Получено: " + film.getName());
             throw new ConditionsNotMetException(s);
         }
         if (film.getDescription().length() > MAX_lENGTH_DESCRIPTION) {
-            String s = "Максимальная длина описания — " + MAX_lENGTH_DESCRIPTION + " символов";
+            final String s = "Максимальная длина описания — " + MAX_lENGTH_DESCRIPTION + " символов";
             log.info("Вызвано исключение: " + s + " Получено: " + film.getDescription().length() + " символов"
                     );
             throw new ConditionsNotMetException(s);
         }
-        if (film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
-            String s = "Дата релиза — не раньше 28 декабря 1895 года";
+        if (film.getReleaseDate().isBefore(DATE_MARK)) {
+            final String s = "Дата релиза — не раньше 28 декабря 1895 года";
             log.info("Вызвано исключение: " + s + " Получено: " + film.getReleaseDate());
             throw new ConditionsNotMetException(s);
         }
-        if (film.getDuration().getSeconds() < 0) {
-            String s = "Продолжительность фильма должна быть положительным числом";
-            log.info("Вызвано исключение: " + s + " Получено: " + film.getDuration().getSeconds());
+        if (film.getDuration() < 0) {
+            final String s = "Продолжительность фильма должна быть положительным числом";
+            log.info("Вызвано исключение: " + s + " Получено: " + film.getDuration());
                         throw new ConditionsNotMetException(s);
         }
-
     }
     private long getNextId() {
         long currentMaxId = films.keySet()
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,12 +1,11 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.ConditionsNotMetException;
 import ru.yandex.practicum.filmorate.exception.DuplicatedDataException;
 import ru.yandex.practicum.filmorate.model.User;
-import org.apache.commons.validator.routines.EmailValidator;
 
 import java.time.LocalDate;
 import java.util.Collection;
@@ -16,7 +15,6 @@ import java.util.Map;
 @RestController
 @RequestMapping("/users")
 @Slf4j
-@Setter
 public class UserController {
 
 
@@ -27,61 +25,62 @@ public class UserController {
         return users.values();
     }
 
-    @PostMapping("/user")
-    public User create(@RequestBody User user) throws ConditionsNotMetException, DuplicatedDataException {
+    @PostMapping
+    public User create(@RequestBody final User user) throws ConditionsNotMetException, DuplicatedDataException {
         validate(user);
         user.setId(getNextId());
         if (user.getName() == null || user.getName().isBlank()) {
             user.setName(user.getLogin());
         }
-        User newUser = users.put(user.getId(), user);
-        log.debug("User created: " + newUser);
-        return newUser;
+        users.put(user.getId(), user);
+        log.debug("User created: " + user);
+        return user;
     }
 
-    @PutMapping("/user")
-    public User update(@RequestBody User user) throws ConditionsNotMetException, DuplicatedDataException {
+    @PutMapping
+    public User update(@RequestBody final User user) throws ConditionsNotMetException, DuplicatedDataException {
         if (user.getId() == null) {
-            throw new ConditionsNotMetException("Id должен быть указан");
+            throw new ConditionsNotMetException("id должен быть указан");
         }
         validate(user);
-        User newUser = users.replace(user.getId(), user);
-        log.debug("User update: " + newUser);
-        return newUser;
+        users.replace(user.getId(), user);
+        log.debug("User update: " + user);
+        return user;
     }
 
-    public void validate(User newUser) throws ConditionsNotMetException, DuplicatedDataException {
+    public void validate(final User newUser) throws ConditionsNotMetException, DuplicatedDataException {
         if (newUser.getEmail() == null || newUser.getEmail().isBlank()) {
-            String s = "Имейл должен быть указан";
+            final String s = "Имейл должен быть указан";
             log.info("Вызвано исключение: " + s + " Пришло: " + newUser.getEmail());
             throw new ConditionsNotMetException(s);
         }
         if (newUser.getEmail().indexOf('@') < 0) {
-            String s = "Имейл должен содержать символ @";
+            final String s = "Имейл должен содержать символ @";
             log.info("Вызвано исключение: " + s + " Пришло: " + newUser.getEmail());
             throw new ConditionsNotMetException(s);
         }
         if(!isValidEmailAddress(newUser.getEmail())){
-            String s = "Не корректно введен Имейл";
+            final String s = "Не корректно введен Имейл";
             log.info("Вызвано исключение: " + s + " Пришло: " + newUser.getEmail());
             throw new ConditionsNotMetException(s);
         }
         if (findEmail(newUser)) {
-            String s = "Этот имейл уже используется";
+            final String s = "Этот имейл уже используется";
             log.info("Вызвано исключение: " + s + " Пришло: " + newUser.getEmail());
             throw new DuplicatedDataException(s);
         }
         if (newUser.getBirthday().isAfter(LocalDate.now())) {
-            String s = "Дата рождения не может быть в будущем";
+            final String s = "Дата рождения не может быть в будущем";
             log.info("Вызвано исключение: " + s + " Пришло: " + newUser.getBirthday());
             throw new ConditionsNotMetException(s);
         }
     }
-    private boolean isValidEmailAddress(String email){
+
+    private boolean isValidEmailAddress(final String email) {
         return EmailValidator.getInstance().isValid(email);
     }
 
-    private boolean findEmail(User newUser) {
+    private boolean findEmail(final User newUser) {
         for (User user : users.values()) {
             if (user.getEmail().equals(newUser.getEmail())) {
                 return true;
@@ -89,7 +88,6 @@ public class UserController {
         }
         return false;
     }
-
     private long getNextId() {
         long currentMaxId = users.keySet()
                 .stream()
