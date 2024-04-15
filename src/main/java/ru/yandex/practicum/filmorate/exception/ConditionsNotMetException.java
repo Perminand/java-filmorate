c12b0f828068061fce7a29589f8739ef47039fd5
Андрей <Perminand@yandex.ru>
2024-04-15 15:22:28 +0300

    refactor: rename class

--- src/main/java/ru/yandex/practicum/filmorate/exception/ConditionsNotMetException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class ConditionsNotMetException extends Throwable {
-    public ConditionsNotMetException(String s) {
-        super(s);
-    }
-}
