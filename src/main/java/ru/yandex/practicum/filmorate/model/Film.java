1bead5fc08668f14a822fb606e79e652c48e67b9
Андрей <Perminand@yandex.ru>
2024-04-15 15:20:28 +0300

    fix: add access

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -8,9 +8,9 @@ import java.time.LocalDate;
 @Data
 @Builder
 public class Film {
-    Long id;
-    String name;
-    String description;
-    LocalDate releaseDate;
-    int duration;
+    private Long id;
+    private String name;
+    private String description;
+    private LocalDate releaseDate;
+    private int duration;
 }
