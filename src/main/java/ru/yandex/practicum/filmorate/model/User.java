1f27595e2deca1db99be82b42783a100bb2b4142
Андрей <Perminand@yandex.ru>
2024-05-01 00:39:57 +0300

    feat: add friends

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,14 +2,16 @@ package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NonNull;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 @Data
-@Builder
+@AllArgsConstructor
 public class User {
     private Long id;
     @NonNull
@@ -21,4 +23,5 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
+    private Set<Long> friends;
 }
