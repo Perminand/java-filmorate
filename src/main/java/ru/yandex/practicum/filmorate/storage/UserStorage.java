8e27f3af795d62aa13c175c6b9090c16b17ef93b
Андрей <Perminand@yandex.ru>
2024-05-01 00:52:48 +0300

    CodeStyle:

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -8,13 +8,14 @@ import java.util.Optional;
 public interface UserStorage extends Storage<User> {
 
     Collection<User> getFriends(long id);
+
     Collection<User> getCommonFriends(long userId, long otherId);
+
     boolean findEmail(final User newUser);
 
     Optional<User> addFriend(long userId, long friendId);
 
     Optional<User> deleteFriend(long userId, long friendId);
-
 
 
 }
