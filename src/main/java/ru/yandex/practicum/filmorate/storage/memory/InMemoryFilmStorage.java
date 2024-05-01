93508460e264bcd6e853353f4219273c70a33aed
Андрей <Perminand@yandex.ru>
2024-05-01 14:59:53 +0300

    feat: add userStorage

--- src/main/java/ru/yandex/practicum/filmorate/storage/memory/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/memory/InMemoryFilmStorage.java
@@ -1,15 +1,28 @@
 package ru.yandex.practicum.filmorate.storage.memory;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.FilmStorage;
+import ru.yandex.practicum.filmorate.storage.UserStorage;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Component
 public class InMemoryFilmStorage extends DataStorage<Film> implements FilmStorage {
+    UserStorage userStorage;
+
+    public InMemoryFilmStorage(){}
+
+    @Autowired
+    public InMemoryFilmStorage(UserStorage userStorage) {
+        this.userStorage = userStorage;
+    }
+
+
 
     @Override
     public Optional<Film> create(Film data) {
@@ -34,16 +47,33 @@ public class InMemoryFilmStorage extends DataStorage<Film> implements FilmStorag
 
     @Override
     public Collection<Film> getPopular(int count) {
-        ArrayList<Film> films = new ArrayList<>(storage.values()
+
+
+        List<Film> films = storage.values()
                 .stream()
-                .sorted((o1, o2) -> Integer.compare(o2.getLikes().size(), o1.getLikes().size()))
-                .limit(count).toList());
+                .sorted(new Comparator<Film>() {
+                    @Override
+                    public int compare(Film o1, Film o2) {
+                        if(o2.getLikes()==null){
+                            if(o1.getLikes()==null){
+                                return 0;
+                            }
+                            return -1;
+                        }
+                        if(o1.getLikes()==null){
+                            return 1;
+                        }
+                        return o2.getLikes().size()-o1.getLikes().size();
+                        }
+                })
+                .limit(10).collect(Collectors.toList());
         return films;
     }
 
     @Override
     public Optional<Film> addLike(long filmId, long userId) {
         findId(filmId);
+        userStorage.findId(userId);
         final Film film = storage.get(filmId);
         Set<Long> likes = film.getLikes();
         if (likes == null) {
@@ -56,6 +86,8 @@ public class InMemoryFilmStorage extends DataStorage<Film> implements FilmStorag
 
     @Override
     public Optional<Film> deleteLike(long filmId, long userId) {
+        findId(filmId);
+        userStorage.findId(userId);
         storage.get(filmId).getLikes().remove(userId);
         return Optional.ofNullable(storage.get(filmId));
     }
