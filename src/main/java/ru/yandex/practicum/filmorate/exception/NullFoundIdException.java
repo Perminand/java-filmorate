9d68a59b623f06c968e084544e64fb850ef5cf20
Андрей <Perminand@yandex.ru>
2024-05-04 11:21:49 +0300

    refactoring: Изменить класс ошибки

--- src/main/java/ru/yandex/practicum/filmorate/exception/NullFoundIdException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.filmorate.exception;
-
-public class NullFoundIdException extends RuntimeException {
-    public NullFoundIdException(String s) {
-        super(s);
-    }
-}
