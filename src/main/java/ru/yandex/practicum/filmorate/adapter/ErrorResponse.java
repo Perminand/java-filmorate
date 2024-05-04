45af9192c86d4309dc973e27a9bfe2c5c21968cb
Андрей <Perminand@yandex.ru>
2024-05-04 11:23:33 +0300

    refactoring: Повестить анатацию @Getter

--- src/main/java/ru/yandex/practicum/filmorate/adapter/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/adapter/ErrorResponse.java
@@ -1,13 +1,12 @@
-package ru.yandex.practicum.filmorate.model;
+package ru.yandex.practicum.filmorate.adapter;
 
+import lombok.Getter;
+
+@Getter
 public class ErrorResponse {
     private final String error;
 
     public ErrorResponse(String error) {
         this.error = error;
-    }
-
-    public String getError() {
-        return error;
     }
 }
