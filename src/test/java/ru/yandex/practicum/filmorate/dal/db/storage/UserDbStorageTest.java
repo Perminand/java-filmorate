67a1c52744716e2475455d8da97b0a2e08948210
andrey <perminand@yandex.ru>
2024-06-08 16:15:53 +0300

    feat: create

---
+++ src/test/java/ru/yandex/practicum/filmorate/dal/db/storage/UserDbStorageTest.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.filmorate.dal.db.storage;
+
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import ru.yandex.practicum.filmorate.dal.mappers.UserRowMapper;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@JdbcTest
+@AutoConfigureTestDatabase
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+@ContextConfiguration(classes = {UserDbStorage.class, UserRowMapper.class})
+class UserDbStorageTest {
+    private final UserDbStorage userDbStorage;
+
+    @Test
+    @Sql(scripts = {"/dataTest.sql"})
+    public void getById() {
+
+        Optional<User> optional = userDbStorage.getById(1);
+
+        assertThat(optional)
+                .isPresent()
+                .hasValueSatisfying(film ->
+                        assertThat(film).hasFieldOrPropertyWithValue("id", 1L)
+                );
+    }
+}
