e103e821bd161403fda715f000c5d28c814027e5
andrey <perminand@yandex.ru>
2024-06-10 19:19:44 +0300

    fix: change

--- src/main/java/ru/yandex/practicum/filmorate/model/FilmJoinGenre.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/FilmJoinGenre.java
@@ -7,8 +7,8 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class FilmJoinGenre {
-    private long id;
+    private long film_id;
+    private Long id;
     private String name;
-    private Long film_id;
 }
 
